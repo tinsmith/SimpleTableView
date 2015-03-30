2eed7bc9285e4544cde06d4fa504854dbc081236
Robert Timmins <timminsr@framework-solutions.com>
2015-03-30 02:29:36 -0400

    First Commit

--- TableViewController.h
+++ TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  Table Views
 //
 //  Created by Robert Timmins on 3/30/15.
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface TableViewController : UITableViewController
 
+@property NSArray *fruits;
 
 @end
-
